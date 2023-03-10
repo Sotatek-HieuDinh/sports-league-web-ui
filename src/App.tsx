77cf007c29c558ebc64357c52f933ef9da8647bb
Sotatek-HieuDinh <hieu.dinh@sotatek.com>
2023-03-10 10:52:31 +0700

    feat: ui leaderboard-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import LeaderBoardPage from "./components/LeaderBoardPage";
 import SchedulePage from "./components/SchedulePage";
 
 function App() {
   return (
     <div className="App">
       <SchedulePage />
+      <LeaderBoardPage />
     </div>
   );
 }
